acf7acb9ac2fc372f606ad843a8d11a9fbbb908e
Hydraxize <hydraxize@gmail.com>
2020-11-19 00:59:30 +0000

    Add project files.

---
+++ ExcelQuant/Core.cs
@@ -0,0 +1,18 @@
+﻿using ExcelDna.Integration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelQuant
+{
+    public class Core
+    {
+        [ExcelFunction(Name = "eqCore.Version", Description = "Runtime version")]
+        public static object Version()
+        {
+            return true;
+        }
+    }
+}
